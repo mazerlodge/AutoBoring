16442a36b333f6cac78dc358fae3d3c9b1ca1f5f
mazerlodge <mazerlodge@hotmail.com>
2023-09-30 17:07:47 -0500

    Added floating point conversion and output.

--- Chapter01/Example01.py
+++ Chapter01/Example01.py
@@ -1,5 +1,7 @@
 #!/bin/python 
 
+# Basic command line IO example.
+
 print('Hello World!') 
 
 print('What is your name?')
@@ -13,3 +15,9 @@ print(len(myName))
 print('What is your age?')
 myAge = input() 
 print('You will be ' + str(int(myAge) + 1 ) + ' in a year.')
+
+print('Enter pi so I can do math on a floating point number:')
+piVal = input() 
+
+newFloat = float(piVal) * 1.42
+print('New float is ' + str(newFloat))
