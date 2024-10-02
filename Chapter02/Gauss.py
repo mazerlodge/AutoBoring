a5dfb7be78f3175f01bc079db2520a27f47a3177
mazerlodge <mazerlodge@hotmail.com>
2024-10-02 15:03:20 -0500

    Expand Chapter 2 examples

---
+++ Chapter02/Gauss.py
@@ -0,0 +1,10 @@
+#!/bin/python 
+
+# https://automatetheboringstuff.com/2e/chapter2/
+# Section Heading 'for Loops and the range() Function'
+
+total = 0 
+for x in range(101): 
+	total = total + x 
+
+print(total) 
