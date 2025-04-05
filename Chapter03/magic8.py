b12e3ea798721aa4a12e52d2dc3e8767bfc183ac
mazerlodge <mazerlodge@hotmail.com>
2025-04-05 09:49:19 -0500

    Expand 8 ball to demo use (and need for) global keyword

--- Chapter03/magic8.py
+++ Chapter03/magic8.py
@@ -5,5 +5,34 @@ magic8answers = ['It is certain',  'It is decidedly so',  'Yes',  'Reply hazy tr
 				 'Ask again later',  'Concentrate and ask again',  'My reply is no',  
 				 'Outlook not so good',  'Very doubtful']
 
-print(magic8answers[random.randint(0,8)])
+answerStats = [0,0,0,0,0,0,0,0]		
+statsTotal = 0		 
 
+def updateStats(idx):
+	# Update the statistics array and total variable
+	# Demo the need for global when updating a variable that isn't an array 
+	
+	global statsTotal 
+	answerStats[idx] += 1
+
+	statsTotal = 0
+	for x in range(8):
+		statsTotal += answerStats[x]
+
+
+def getAnswer(): 
+	# Retrieve a random answer from the magic 8 ball 
+
+	answerIdx = random.randint(0,7)
+	updateStats(answerIdx)
+			
+	return magic8answers[answerIdx]
+
+
+# call the 8 ball multiple times to load the stats array with some random data
+for x in range(42):
+	getAnswer()
+
+print(getAnswer())
+
+print(answerStats, statsTotal)
