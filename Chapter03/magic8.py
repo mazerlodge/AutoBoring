5af02c0212102f8b73d35a943e2d273d77440cc5
mazerlodge <mazerlodge@hotmail.com>
2025-04-05 09:32:55 -0500

    Initial version.

---
+++ Chapter03/magic8.py
@@ -0,0 +1,9 @@
+
+import random 
+
+magic8answers = ['It is certain',  'It is decidedly so',  'Yes',  'Reply hazy try again',  
+				 'Ask again later',  'Concentrate and ask again',  'My reply is no',  
+				 'Outlook not so good',  'Very doubtful']
+
+print(magic8answers[random.randint(0,8)])
+
